f30adf4ea5c0abf6c462743a5905617d759532f6
Stringxy <274343534@qq.com>
2018-06-20 10:28:01 +0800

    0620

---
+++ unity-3d/c#/撞方块demo/ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ball : MonoBehaviour {
+	public int _speed =500;
+	public Text _text;
+	public GameObject _bricksParent;
+	public GameObject img;
+	private int _brickCount;
+	// Use this for initialization
+	void Start () {
+		
+		float x = Random.Range (-100, 100) / 100f;
+		float y = Random.Range (-100, 100) / 100f;
+		var rigidbody = GetComponent<Rigidbody> ();
+		rigidbody.AddForce (new Vector3 (x, y, 0) * _speed);
+		_brickCount = _bricksParent.transform.childCount;
+		_text.text = "剩余：" + _brickCount;
+	}
+
+	private void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "brick") {
+			GameObject.Destroy (collision.gameObject);
+			_brickCount -= 1;
+			if (_brickCount == 0) {
+				img.SetActive (true);
+			}
+			_text.text = "剩余：" + _brickCount;
+		}
+	}
+}
---
+++ unity-3d/c#/撞方块demo/catcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catcher : MonoBehaviour
+{
+
+	public  GameObject _leftWall;
+	public GameObject _rightWall;
+	public float _catcherSpeed = 0.2f;
+
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.A) && _leftWall.transform.position.x < (transform.position.x - 1.2f)) {
+			transform.localPosition = new Vector3 (transform.localPosition.x - _catcherSpeed, transform.localPosition.y, transform.localPosition.z);
+
+		} else if (Input.GetKey (KeyCode.D) && _rightWall.transform.position.x > (transform.position.x + 1.2f)) {
+			transform.localPosition = new Vector3 (transform.localPosition.x + _catcherSpeed, transform.localPosition.y, transform.localPosition.z);
+		}
+
+	}
+
+}
